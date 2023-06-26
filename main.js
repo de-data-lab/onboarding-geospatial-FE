96f84dcfa4e97d0c22721b5a8c15062f3fb68aed
Dennis Anderson <dennisanderson@Denniss-MacBook-Pro.local>
2023-06-26 09:19:03 -0400

    tileLayer not appearing

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// code goes in here 
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const map = L.map('map').setView([39.2,-75.523],9)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+// 	maxZoom: 19,
+// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+// }).addTo(map);
