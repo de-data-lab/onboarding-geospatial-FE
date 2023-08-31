2d0f497dc325b3d0c1b5b7eed0fd836734b0ba22
Christy Grothues <122482422+Khryssty@users.noreply.github.com>
2023-08-31 13:28:40 -0400

    Geospatial Onboarding 2023 - Build a map

--- main.js
+++ main.js
@@ -1,9 +1,138 @@
+// import * as L from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+// //import { getColorScale } from './utils';
+// import { getColorScale, drawLegend, drawMap } from './utils';
+
+// const map = L.map('map').setView([39.2,-75.523],9)
+// L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
+// // L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+// // 	maxZoom: 19,
+// // 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+// // }).addTo(map);
+
+// let variable = "medincome"; 
+// let colorScheme = "interpolateBlues";
+
+// fetch("public/de-data.geojson")
+// .then(res => res.json())
+// .then(data => {
+// //  console.log(data)
+//     const colScale = getColorScale(data, variable, colorScheme); 
+//     L.geoJSON(data,{
+//         style: function (feature) {
+//             return {
+//                 weight: .3,
+//                 color: "black",
+//                 fillColor: colScale(feature.properties[variable]),
+//                 fillOpacity: .8
+//             }
+//         }
+//     }  )
+    
+//     .bindTooltip(function (layer) {
+//         return layer.feature.properties.NAME
+//       })
+//     .addTo(map)
+
+//     drawLegend(colorScheme);
+//     document.querySelector("#legend-title").innerText = variable;
+// })
+
+// // drawLegend(colorScheme);
+// // document.querySelector("#legend-title").innerText = variable;
+
+// document.addEventListener("DOMContentLoaded", () => {
+      
+//     document.querySelector("#options").addEventListener("change", (e) => {
+//       variable = e.target.value;
+//       drawMap(variable, colorScheme);
+//     });
+  
+//     document.querySelector("#colorScheme").addEventListener("change", (e) => {
+//       colorScheme = e.target.value;
+//       drawMap(variable, colorScheme);
+//     });
+
+//     drawMap(variable, colorScheme);
+
+//   });
+
+
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { getColorScale, drawLegend } from './utils';
 
-const map = L.map('map').setView([39.2,-75.523],9)
+const map = L.map('map').setView([39.2, -75.523], 9);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
-// L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-// 	maxZoom: 19,
-// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-// }).addTo(map);
+
+
+
+let variable = "medincome";
+let colorScheme = "interpolateBlues";
+
+function drawMap() {
+    fetch("de-data.geojson")
+    .then(res => res.json())
+    .then(data => {
+  
+      const colScale = getColorScale(data, variable, colorScheme); 
+  
+      L.geoJSON(data, {
+            style: function (feature) {
+                return { 
+                  weight: .3,
+                  color: "black",
+                  fillColor: colScale(feature.properties[variable]),
+                  fillOpacity: .8           
+                }
+            }
+          })
+        .bindTooltip(function (layer) {
+          return layer.feature.properties.NAME
+        })
+        .addTo(map)
+    })
+    drawLegend(colorScheme);
+    
+    document.querySelector("#legend-title").innerText = variable;
+  
+  }
+
+// fetch("public/de-data.geojson")
+//   .then(res => res.json())
+//   .then(data => {
+//     const colScale = getColorScale(data, variable, colorScheme);
+//     L.geoJSON(data, {
+//       style: function (feature) {
+//         return {
+//           weight: .3,
+//           color: "black",
+//           fillColor: colScale(feature.properties[variable]),
+//           fillOpacity: .8
+//         }
+//       }
+//     })
+//     .bindTooltip(function (layer) {
+//       return layer.feature.properties.NAME;
+//     })
+//     .addTo(map);
+
+//     drawLegend(colorScheme);
+//     document.querySelector("#legend-title").innerText = variable;
+//   });
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#options").addEventListener("change", (e) => {
+    variable = e.target.value;
+    drawMap();
+  });
+
+  document.querySelector("#colorScheme").addEventListener("change", (e) => {
+    colorScheme = e.target.value;
+    drawMap();
+  });
+
+  drawMap();
+});
+
+
--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 
+
 export function drawLegend(colorScheme) {
   const legendLabels = ["Lowest", "Low", "Mid-Point", "High", "Highest"]
   const legendEntries = d3.quantize(d3[colorScheme], 5).map((color, i)  => {
@@ -24,3 +25,41 @@ export function getColorScale(data, key, colorScheme) {
           .range(d3.quantize(d3[colorScheme], 5))
   return colScale; 
 }
+
+// export function drawMap(variable, colorScheme) {
+//   fetch("public/de-data.geojson")
+//   .then(res => res.json())
+//   .then(data => {
+
+//     const colScale = getColorScale(data, variable, colorScheme); 
+
+//     map.eachLayer((layer) => {
+//       if (layer !== map) {
+//         map.removeLayer(layer);
+//       }
+//     });
+
+//     L.geoJSON(data, {
+//           style: function (feature) {
+//               return { 
+//                 weight: .3,
+//                 color: "black",
+//                 fillColor: colScale(feature.properties[variable]),
+//                 fillOpacity: .8           
+//               }
+//           }
+//         })
+//       .bindTooltip(function (layer) {
+//         return 
+//         `<strong>${layer.feature.properties.NAME}:</strong>
+//         <br/>
+//          ${variable}: <strong>${layer.feature.properties[variable].toLocaleString()}</strong>`;
+//       })
+//       .addTo(map)
+
+//       drawLegend(colorScheme);
+//       document.querySelector("#legend-title").innerText = variable;
+//   });
+
+  
+  
